7595369ef801cfd0964b8e362f38ccd396491783
KadZumi77 <yazykova.2004@mail.ru>
2023-12-20 23:29:26 +0300

    Добавила ObjReader и ObjWriter

--- src/main/java/com/cgvsu/model/Model.java
+++ src/main/java/com/cgvsu/model/Model.java
@@ -12,4 +12,46 @@ public class Model {
     public ArrayList<Vector2f> textureVertices = new ArrayList<Vector2f>();
     public ArrayList<Vector3f> normals = new ArrayList<Vector3f>();
     public ArrayList<Polygon> polygons = new ArrayList<Polygon>();
+
+    public Model(ArrayList<Vector3f> vertices, ArrayList<Vector2f> textureVertices, ArrayList<Vector3f> normals, ArrayList<Polygon> polygons) {
+        this.vertices = vertices;
+        this.textureVertices = textureVertices;
+        this.normals = normals;
+        this.polygons = polygons;
+    }
+
+    public List<Vector3f> getVertices() {
+        return new ArrayList<>(vertices);
+    }
+    public void addVertex(Vector3f vertex) {
+        this.vertices.add(vertex);
+    }
+
+    public ArrayList<Vector2f> getTextureVertices() {
+        return new ArrayList<>(textureVertices);
+    }
+
+    public void addTextureVertex(Vector2f textureVertex) {
+        this.textureVertices.add(textureVertex);
+    }
+
+    public ArrayList<Vector3f> getNormals() {
+        return new ArrayList<>(normals);
+    }
+
+    public void addNormal(Vector3f normal) {
+        this.normals.add(normal);
+    }
+
+    public ArrayList<Polygon> getPolygons() {
+        return new ArrayList<>(polygons);
+    }
+
+    public void addPolygon(Polygon polygon) {
+        this.polygons.add(polygon);
+    }
+    public boolean isEmpty() {
+        return vertices.isEmpty();
+    }
+
 }
