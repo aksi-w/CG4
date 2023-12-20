944dd30dbc4542345263b2543b4c25ce1012a17e
Polina Popova <popovapolina2133@gmail.com>
2023-12-20 17:27:09 +0300

    добавила класс Math

---
+++ src/main/java/com/cgvsu/math/Matrix/Matrix3f.java
@@ -0,0 +1,101 @@
+package com.cgvsu.math.Matrix;
+
+
+import com.cgvsu.math.Vector.Vector3f;
+
+public class Matrix3f {
+    private static final float eps = 1e-7f;
+
+    private final float[][] matrix;
+
+    public float getMatrix(int i, int j) {
+        return this.matrix[i][j];
+    }
+
+    public Matrix3f(float[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public boolean equals(Matrix3f otherMatrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (Math.abs(matrix[i][j] - otherMatrix.matrix[i][j]) < eps) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static Matrix3f addition(Matrix3f matrix1, Matrix3f matrix2) {
+        float[][] res = new float[3][3];
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res.length; j++) {
+                res[i][j] = matrix1.matrix[i][j] + matrix2.matrix[i][j];
+            }
+        }
+        return new Matrix3f(res);
+    }
+
+    public static Matrix3f subtraction(Matrix3f matrix1, Matrix3f matrix2) {
+        float[][] res = new float[3][3];
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res.length; j++) {
+                res[i][j] = matrix1.matrix[i][j] - matrix2.matrix[i][j];
+            }
+        }
+        return new Matrix3f(res);
+    }
+
+    public static Matrix3f multiplication(Matrix3f matrix1, Matrix3f matrix2) {
+        float[][] res = new float[3][3];
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res.length; j++) {
+                for (int k = 0; k < res.length; k++) {
+                    res[i][j] += matrix1.matrix[i][k] * matrix2.matrix[k][j];
+                }
+            }
+        }
+        return new Matrix3f(res);
+    }
+
+    public static Matrix3f transposition(Matrix3f matrix) {
+        float[][] res = new float[3][3];
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res.length; j++) {
+                res[i][j] = matrix.matrix[j][i];
+            }
+        }
+        return new Matrix3f(res);
+    }
+
+    public static Matrix3f zeroMatrix() {
+        float[][] zeroMatrix = new float[3][3];
+        return new Matrix3f(zeroMatrix);
+    }
+
+    public static Matrix3f unitMatrix() {
+        float[][] res = new float[3][3];
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res.length; j++) {
+                if (i == j) {
+                    res[i][j] = 1;
+                } else {
+                    res[i][j] = 0;
+                }
+            }
+        }
+        return new Matrix3f(res);
+    }
+
+    public static Vector3f multiplyOnVector(Matrix3f matrix, Vector3f vector) {
+        float[] res = new float[3];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = matrix.matrix[i][0] * vector.getX() +
+                    matrix.matrix[i][1] * vector.getY() +
+                    matrix.matrix[i][2] * vector.getZ();
+        }
+        return new Vector3f(res[0], res[1], res[2]);
+    }
+}
+
---
+++ src/main/java/com/cgvsu/math/Vector/Vector3f.java
@@ -0,0 +1,92 @@
+package com.cgvsu.math.Vector;
+
+public class Vector3f {
+
+    private final float x;
+    private final float y;
+    private final float z;
+    private static final float eps = 1e-7f;
+
+    public Vector3f(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public boolean equals(Vector3f other) {
+        return Math.abs(x - other.getX()) < eps && Math.abs(y - other.getY()) < eps && Math.abs(z - other.getZ()) < eps;
+    }
+
+    public static Vector3f addition(Vector3f vector1, Vector3f vector2) {
+        float resX = vector1.getX() + vector2.getX();
+        float resY = vector1.getY() + vector2.getY();
+        float resZ = vector1.getZ() + vector2.getZ();
+        return new Vector3f(resX, resY, resZ);
+    }
+
+    public static Vector3f subtraction(Vector3f vector1, Vector3f vector2) {
+        float resX = vector1.getX() - vector2.getX();
+        float resY = vector1.getY() - vector2.getY();
+        float resZ = vector1.getZ() - vector2.getZ();
+        return new Vector3f(resX, resY, resZ);
+    }
+
+    public static float lengthVector(Vector3f vector) {
+        return (float) Math.sqrt(vector.getX() * vector.getX() + vector.getY() * vector.getY() + vector.getZ() * vector.getZ());
+    }
+
+    public static Vector3f normalize(Vector3f vector) {
+        float length = lengthVector(vector);
+        if (length > eps) {
+            float normalizedX = vector.getX() / length;
+            float normalizedY = vector.getY() / length;
+            float normalizedZ = vector.getZ() / length;
+            return new Vector3f(normalizedX, normalizedY, normalizedZ);
+        } else {
+            throw new IllegalArgumentException("Деление на 0!");
+        }
+    }
+
+    public static Vector3f multiplication(Vector3f vector, float a) {
+        float resX = vector.getX() * a;
+        float resY = vector.getY() * a;
+        float resZ = vector.getZ() * a;
+        return new Vector3f(resX, resY, resZ);
+    }
+
+    public static Vector3f division(Vector3f vector, float a) {
+        if (a < eps) {
+            throw new IllegalArgumentException("Деление на 0!");
+        } else {
+            float resX = vector.getX() / a;
+            float resY = vector.getY() / a;
+            float resZ = vector.getZ() / a;
+            return new Vector3f(resX, resY, resZ);
+        }
+    }
+
+     public static float scalar(Vector3f vector1, Vector3f vector2) {
+        return vector1.getX() * vector2.getX() + vector1.getY() * vector2.getY() + vector1.getZ() * vector2.getZ();
+    }
+
+    public Vector3f cross(Vector3f other) {
+        float resX = this.y * other.z - this.z * other.y;
+        float resY = this.z * other.x - this.x * other.z;
+        float resZ = this.x * other.y - this.y * other.x;
+
+        return new Vector3f(resX, resY, resZ);
+    }
+
+}
